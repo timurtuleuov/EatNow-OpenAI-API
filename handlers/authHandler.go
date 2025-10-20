4781ce9b8f979119a779ff525c6197448b9ebd94
timurtuleuov <timmmur123@gmail.com>
2025-10-21 02:11:35 +0500

    create user

--- handlers/authHandler.go
+++ handlers/authHandler.go
@@ -1,5 +1,26 @@
+package handlers
 
+import (
+	"database/sql"
+	"fmt"
 
-func createUser() {
+	"golang.org/x/crypto/bcrypt"
+)
+
+func createUser(db *sql.DB, username string, email string, password string, platform string) (bool, error) {
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return false, fmt.Errorf("failed to hash password: %w", err)
+	}
+
+	_, err = db.Exec(`
+		INSERT INTO users (name, password, email, platform)
+        VALUES ($1, $2, $3, $4)
+	`, username, string(hashedPassword), email, platform)
+	if err != nil {
+		return false, fmt.Errorf("failed to insert user: %w", err)
+	}
+	return true, nil
 
 }
